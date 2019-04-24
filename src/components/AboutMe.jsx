a937ec309f57e3a4c969da0de831a90bc937067d
alfonsodumlao <alfonso.dumlao@gmail.com>
2019-04-24 13:00:39 -0400

    changed pdf file edits

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = (props) => {
        alt="aboutme" />
 
       <div className="about-blurbs">
-        <p className="about-p">Alfonso Dumlao is a web developer from Brooklyn.  He enjoys creating sites and applications that have clean layouts, visual appeal, have smart functionality, and are user-intuitive.  With an extensive background in flagship retail management- goal-setting, calendar-planning and team-motivating come second nature to him.  Having lived and breathed visual merchandising and styling for over a decade, attention to detail and flair translate into the products he builds.</p>
+        <p className="about-p">Alfonso Dumlao is a web developer from Brooklyn.  He enjoys creating sites and applications that have clean layouts, visual appeal, smart functionality, and are user-intuitive.  With an extensive background in flagship retail management- goal-setting, calendar-planning and team-motivating come second nature to him.  Having lived and breathed visual merchandising and styling for over a decade, attention to detail and flair translate into the products he builds.</p>
 
         <p className="about-p">In his free time, he likes to go camping, do yoga, and sit by the fireplace with his husband, Cameron, and cat, Nina.</p>
 
